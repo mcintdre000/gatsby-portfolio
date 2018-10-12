f08999fc62eca24c2ff75c9064310c7f6ab5ee9a
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-11 23:29:33 -0700

    Test

--- src/components/Main.js
+++ src/components/Main.js
@@ -141,21 +141,21 @@ Technologies used:</p>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" method="POST" netlify>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" />
+              <input type="text" name="email" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
             <ul className="actions">
-              <li><button type="submit" className="special"></button></li>
+              <li><input type="submit" value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
           </form>
