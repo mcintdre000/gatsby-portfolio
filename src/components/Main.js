d0e5ecb17d014a3e5894e610644f65e06f9a40ff
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-12 23:43:31 -0700

    Form submission test

--- src/components/Main.js
+++ src/components/Main.js
@@ -143,19 +143,19 @@ Technologies used:</p>
           <h2 className="major">Contact</h2>
           <form name="contact" method="POST" netlify>
             <div className="field half first">
-              <label htmlFor="name">Name</label>
+              <label>Name</label>
               <input type="text" name="name" id="name" />
             </div>
             <div className="field half">
-              <label htmlFor="email">Email</label>
+              <label>Email</label>
               <input type="text" name="email" id="email" />
             </div>
             <div className="field">
-              <label htmlFor="message">Message</label>
+              <label>Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
+              <li><input type="submit" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
             </ul>
           </form>
