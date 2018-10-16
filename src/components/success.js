a7a6b197ad15c523065a080b117cd7c56ec64fe4
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-16 16:01:43 -0700

    Form Success page test

--- src/components/success.js
+++ src/components/success.js
@@ -1,28 +1,18 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import Layout from './layout'
-
-import pic11 from '../assets/images/pic11.jpg'
 
 const Success = (props) => (
-    <Layout>
+    <div>
         <Helmet>
             <title>Success Page</title>
             <meta name="description" content="Success Page" />
         </Helmet>
 
-        <div id="main" className="alt">
-            <section id="one">
-                <div className="inner">
-                    <header className="major">
-                        <h1>Success/Thank You Page</h1>
-                    </header>
-                    <span className="image main"><img src={pic11} alt="" /></span>
-                    <p>Thank you for contacting us!</p>
-                </div>
-            </section>
+        <div>
+            <h1>Thank you!</h1>
+            <p>This is a custom thank you page for form submissions</p>
         </div>
-    </Layout>
+    </div>
 )
 
 export default Success
