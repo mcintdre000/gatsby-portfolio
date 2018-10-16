6ffaed5531a7f3b7980552d4c626177deb3db12a
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-16 14:10:49 -0700

    Form Test

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,29 @@ import pic06 from '../images/about-pic.jpg'
 
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .catch(error => alert(error));
+  };
+
   render() {
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
@@ -141,19 +164,32 @@ Technologies used:</p>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form 
+            name="contact" 
+            method="POST" 
+            data-netlify="true" 
+            data-netlify-honeypot="bot-field" 
+            onSubmit={this.handleSubmit}
+          >
             <div className="field half first">
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
             <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={this.handleChange} />
+              </label>
+            </p>
               <label>Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" onChange={this.handleChange} />
             </div>
             <div className="field half">
               <label>Email</label>
-              <input type="email" name="email" id="email" />
+              <input type="email" name="email" id="email" onChange={this.handleChange} />
             </div>
             <div className="field">
               <label>Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" onChange={this.handleChange}></textarea>
             </div>
             <ul className="actions">
               <li><input type="submit" className="special" /></li>
